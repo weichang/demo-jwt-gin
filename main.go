90d75d89ce566e3d7d9899e82ef6e8b2c2350bbc
Jeffrey <jeffrey.kuo@cloud-interactive.com>
2021-10-25 15:36:05 +0800

    Add main, user, repository

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func main()  {
+
+	db,err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	if err := db.AutoMigrate(&User{}); err != nil {
+		panic(err)
+	}
+	DB = db
+	_, _ = insertUser("Jeffrey", "12345", 30)
+	_, _ = insertUser("David", "12345", 25)
+
+}
